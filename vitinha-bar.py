a4163ba890f3e87db7c575204bc319217dc235b3
Debatra Chatterjee <debatra.chatterjee.24@gmail.com>
2022-04-30 12:47:45 +0530

    Bar chart

--- vitinha-bar.py
+++ vitinha-bar.py
@@ -18,45 +18,43 @@ final_vitinha_data.round(1)
 # x_axis_count = [0,1,2,3,4]
 # # print(hist_dataframe) 
 
-# # plt.hist(hist_dataframe)
-# # plt.xticks(x_axis_count[:-1], x_axis)
-# # plt.show()
-
-# bar_data = final_vitinha_data[final_vitinha_data["Legend"].isin(["Tackle success %", "Aerial success %", "Dribble Success %", "Pass Accuracy"])]
-# bar_data_final = bar_data["Unnamed: 1"].apply(lambda x: x*100)
-# bar_data.drop("Unnamed: 1", axis=1, inplace=True)
-# bar_data["Values"] = bar_data_final
-# bar_data.round({"Values":1})
-# # print(bar_data)
-
-
-
-# f = plt.figure(facecolor="#000")
-# f.set_figwidth(5)
-# f.set_figheight(3)
-
-# # plt.bar(x= np.arange(1,5), height=bar_data["Values"], width=0.3)
-# # plt.yticks(np.arange(0,100,10))
-# # plt.xticks(np.arange(1,5),bar_data["Legend"],fontsize=8)
-
-# ax=plt.axes()
-# ax.set_facecolor("#000")
-# ax.bar(x= np.arange(1,5), height=bar_data["Values"], width=0.4, color="#0B0")
-# plt.yticks(np.arange(0,100,10), color="#fff")
-# plt.xticks(np.arange(1,5),bar_data["Legend"],fontsize=8, color="#fff")
-
-# # plt.xlim((0,4))
-# # plt.xticks(bar_data["Legend"])
-# # plt.xticks(np.arange(1,4))
-# # plt.ylim((0,10))
-# # plt.yticks(np.arange(0,100))
-
-# for index, value in enumerate(bar_data["Values"]):
-#     print(value)
-#     print(index)
-#     text = plt.text(index+1,value,str(value)+"%",color="#fff", fontweight="bold", fontsize=8)
-#     text.set_position((index+0.805,value-5))
-
-# # plt.bar_label(bar_data["Values"])
-
+# plt.hist(hist_dataframe)
+# plt.xticks(x_axis_count[:-1], x_axis)
 # plt.show()
+
+bar_data = final_vitinha_data[final_vitinha_data["Legend"].isin(["Tackle success %", "Aerial success %", "Dribble Success %", "Pass Accuracy"])]
+bar_data_final = bar_data["Unnamed: 1"].apply(lambda x: x*100)
+bar_data.drop("Unnamed: 1", axis=1, inplace=True)
+bar_data["Values"] = bar_data_final
+bar_data.round({"Values":1})
+# print(bar_data)
+
+
+
+f = plt.figure(facecolor="#000")
+f.set_figwidth(5)
+f.set_figheight(3)
+
+# plt.bar(x= np.arange(1,5), height=bar_data["Values"], width=0.3)
+# plt.yticks(np.arange(0,100,10))
+# plt.xticks(np.arange(1,5),bar_data["Legend"],fontsize=8)
+
+ax=plt.axes()
+ax.set_facecolor("#000")
+ax.bar(x= np.arange(1,5), height=bar_data["Values"], width=0.4, color="#0B0")
+plt.yticks(np.arange(0,100,10), color="#fff")
+plt.xticks(np.arange(1,5),bar_data["Legend"],fontsize=8, color="#fff")
+
+# plt.xlim((0,4))
+# plt.xticks(bar_data["Legend"])
+# plt.xticks(np.arange(1,4))
+# plt.ylim((0,10))
+# plt.yticks(np.arange(0,100))
+
+for index, value in enumerate(bar_data["Values"]):
+    text = plt.text(index+1,value,str(value)+"%",color="#fff", fontweight="bold", fontsize=8)
+    text.set_position((index+0.805,value-5))
+
+# plt.bar_label(bar_data["Values"])
+
+plt.show()
