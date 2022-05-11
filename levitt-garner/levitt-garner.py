0561d955962c42732d3d77aadb8d4c2e2b6bf720
Debatra Chatterjee <debatra.chatterjee.24@gmail.com>
2022-05-11 20:22:38 +0530

    Levitt and Garner comparison - Radar and Bar

--- levitt-garner/levitt-garner.py
+++ levitt-garner/levitt-garner.py
@@ -1,3 +1,5 @@
+#This code won't work out of the box. Comment any two of defence, possession and passing to run the uncommented code.
+
 import numpy as np
 import pandas as pd
 from math import pi
@@ -26,130 +28,130 @@ for i in range(len(all_data["Dylan Levitt"])):
 
 #Defence
 
-# levitt_data = {}
-# garner_data = {}
+levitt_data = {}
+garner_data = {}
 
-# for i in range(4,6):
-#     levitt_data[all_data["Attribute"][i]] = all_data["Dylan Levitt"][i]
-#     garner_data[all_data["Attribute"][i]] = all_data["James Garner"][i]
+for i in range(4,6):
+    levitt_data[all_data["Attribute"][i]] = all_data["Dylan Levitt"][i]
+    garner_data[all_data["Attribute"][i]] = all_data["James Garner"][i]
 
-# for i in range(7,12):
-#     levitt_data[all_data["Attribute"][i]] = all_data["Dylan Levitt"][i]
-#     garner_data[all_data["Attribute"][i]] = all_data["James Garner"][i]
+for i in range(7,12):
+    levitt_data[all_data["Attribute"][i]] = all_data["Dylan Levitt"][i]
+    garner_data[all_data["Attribute"][i]] = all_data["James Garner"][i]
 
-# radar_dataframe = pd.DataFrame([levitt_data, garner_data], index=["Dylan Levitt", "James Garner"])
-# # print(radar_dataframe)
+radar_dataframe = pd.DataFrame([levitt_data, garner_data], index=["Dylan Levitt", "James Garner"])
+# print(radar_dataframe)
 
-# Attributes = list(radar_dataframe)
-# AttNo = len(Attributes)
+Attributes = list(radar_dataframe)
+AttNo = len(Attributes)
 
-# values_levitt = radar_dataframe.iloc[0].tolist()
-# values_levitt += values_levitt[:1]
-# values_garner = radar_dataframe.iloc[1].tolist()
-# values_garner += values_garner[:1]
+values_levitt = radar_dataframe.iloc[0].tolist()
+values_levitt += values_levitt[:1]
+values_garner = radar_dataframe.iloc[1].tolist()
+values_garner += values_garner[:1]
 
-# angles = [n / float(AttNo) * 2 * pi for n in range(AttNo)]
-# angles += angles [:1]
+angles = [n / float(AttNo) * 2 * pi for n in range(AttNo)]
+angles += angles [:1]
 
-# plt.figure(facecolor="#fff")
-# ax = plt.subplot(131, polar=True)
-# # ax.spines['polar'].set_color('#fff')
-# ax.spines['polar'].set_visible(False)
+plt.figure(facecolor="#fff")
+ax = plt.subplot(131, polar=True)
+# ax.spines['polar'].set_color('#fff')
+ax.spines['polar'].set_visible(False)
 
-# ax.set_xticks(angles[:-1], Attributes, color="#000",fontsize=7)
+ax.set_xticks(angles[:-1], Attributes, color="#000",fontsize=7)
 
-# ax.plot(angles,values_levitt, color="#004768")
-# ax.plot(angles,values_garner, color="#c68100")
+ax.plot(angles,values_levitt, color="#004768")
+ax.plot(angles,values_garner, color="#c68100")
 
-# # ax.fill(angles, values_levitt, '#004768')
-# # ax.fill(angles, values_garner, '#c68100', alpha=0.5)
+# ax.fill(angles, values_levitt, '#004768')
+# ax.fill(angles, values_garner, '#c68100', alpha=0.5)
 
-# # ax.set_title("Levitt", color="#fff", fontsize=9)
-# # plt.legend()
+# ax.set_title("Levitt", color="#fff", fontsize=9)
+# plt.legend()
 
-# ax2 = plt.subplot(133)
-# ax2.spines["top"].set_visible(False)
-# ax2.spines["right"].set_visible(False)
+ax2 = plt.subplot(133)
+ax2.spines["top"].set_visible(False)
+ax2.spines["right"].set_visible(False)
 
-# x=np.arange(1,3)
-# bar_data_levitt = []
-# bar_data_garner = []
+x=np.arange(1,3)
+bar_data_levitt = []
+bar_data_garner = []
 
-# bar_data_levitt.append(all_data.iloc[6]["Dylan Levitt"])
-# bar_data_levitt.append(all_data.iloc[12]["Dylan Levitt"])
+bar_data_levitt.append(all_data.iloc[6]["Dylan Levitt"])
+bar_data_levitt.append(all_data.iloc[12]["Dylan Levitt"])
 
-# bar_data_garner.append(all_data.iloc[6]["James Garner"])
-# bar_data_garner.append(all_data.iloc[12]["James Garner"])
+bar_data_garner.append(all_data.iloc[6]["James Garner"])
+bar_data_garner.append(all_data.iloc[12]["James Garner"])
 
-# # print(bar_data_garner)
+# print(bar_data_garner)
 
-# ax2.bar(x-0.1, height=bar_data_levitt, width=0.2, color="#004768", alpha=0.75)
-# ax2.bar(x+0.1, height=bar_data_garner, width=0.2, color="#c68100", alpha=0.75)
+ax2.bar(x-0.1, height=bar_data_levitt, width=0.2, color="#004768", alpha=0.75)
+ax2.bar(x+0.1, height=bar_data_garner, width=0.2, color="#c68100", alpha=0.75)
 
-# ax2.set_yticks(np.arange(0,90,10))
+ax2.set_yticks(np.arange(0,90,10))
 
-# plt.show()
+plt.show()
 
 # Possession
 
-# levitt_data = {}
-# garner_data = {}
+levitt_data = {}
+garner_data = {}
 
-# for i in range(13,19):
-#     levitt_data[all_data["Attribute"][i]] = all_data["Dylan Levitt"][i]
-#     garner_data[all_data["Attribute"][i]] = all_data["James Garner"][i]
+for i in range(13,19):
+    levitt_data[all_data["Attribute"][i]] = all_data["Dylan Levitt"][i]
+    garner_data[all_data["Attribute"][i]] = all_data["James Garner"][i]
 
-# radar_dataframe = pd.DataFrame([levitt_data, garner_data], index=["Dylan Levitt", "James Garner"])
-# # print(radar_dataframe)
+radar_dataframe = pd.DataFrame([levitt_data, garner_data], index=["Dylan Levitt", "James Garner"])
+# print(radar_dataframe)
 
-# Attributes = list(radar_dataframe)
-# AttNo = len(Attributes)
+Attributes = list(radar_dataframe)
+AttNo = len(Attributes)
 
-# values_levitt = radar_dataframe.iloc[0].tolist()
-# values_levitt += values_levitt[:1]
-# values_garner = radar_dataframe.iloc[1].tolist()
-# values_garner += values_garner[:1]
+values_levitt = radar_dataframe.iloc[0].tolist()
+values_levitt += values_levitt[:1]
+values_garner = radar_dataframe.iloc[1].tolist()
+values_garner += values_garner[:1]
 
-# angles = [n / float(AttNo) * 2 * pi for n in range(AttNo)]
-# angles += angles [:1]
+angles = [n / float(AttNo) * 2 * pi for n in range(AttNo)]
+angles += angles [:1]
 
-# plt.figure(facecolor="#fff")
-# ax = plt.subplot(131, polar=True)
-# # ax.spines['polar'].set_color('#fff')
-# ax.spines['polar'].set_visible(False)
+plt.figure(facecolor="#fff")
+ax = plt.subplot(131, polar=True)
+# ax.spines['polar'].set_color('#fff')
+ax.spines['polar'].set_visible(False)
 
-# ax.set_xticks(angles[:-1], Attributes, color="#000",fontsize=7)
+ax.set_xticks(angles[:-1], Attributes, color="#000",fontsize=7)
 
-# ax.plot(angles,values_levitt, color="#004768")
-# ax.plot(angles,values_garner, color="#c68100")
+ax.plot(angles,values_levitt, color="#004768")
+ax.plot(angles,values_garner, color="#c68100")
 
-# # ax.fill(angles, values_levitt, '#004768')
-# # ax.fill(angles, values_garner, '#c68100', alpha=0.5)
+# ax.fill(angles, values_levitt, '#004768')
+# ax.fill(angles, values_garner, '#c68100', alpha=0.5)
 
-# # ax.set_title("Levitt", color="#fff", fontsize=9)
-# # plt.legend()
+# ax.set_title("Levitt", color="#fff", fontsize=9)
+# plt.legend()
 
-# ax2 = plt.subplot(133)
-# ax2.spines["top"].set_visible(False)
-# ax2.spines["right"].set_visible(False)
+ax2 = plt.subplot(133)
+ax2.spines["top"].set_visible(False)
+ax2.spines["right"].set_visible(False)
 
-# x=np.arange(1,2)
-# bar_data_levitt = []
-# bar_data_garner = []
+x=np.arange(1,2)
+bar_data_levitt = []
+bar_data_garner = []
 
-# bar_data_levitt.append(all_data.iloc[19]["Dylan Levitt"])
-# bar_data_garner.append(all_data.iloc[19]["James Garner"])
+bar_data_levitt.append(all_data.iloc[19]["Dylan Levitt"])
+bar_data_garner.append(all_data.iloc[19]["James Garner"])
 
 
-# # print(bar_data_garner)
+# print(bar_data_garner)
 
-# ax2.bar(x=1.025, height=bar_data_levitt, width=0.05, color="#004768", alpha=0.75)
-# ax2.bar(x=0.975, height=bar_data_garner, width=0.05, color="#c68100", alpha=0.75)
+ax2.bar(x=1.025, height=bar_data_levitt, width=0.05, color="#004768", alpha=0.75)
+ax2.bar(x=0.975, height=bar_data_garner, width=0.05, color="#c68100", alpha=0.75)
 
-# ax2.set_xticks(np.arange(0.9,1.2,0.1))
-# ax2.set_yticks(np.arange(0,90,10))
+ax2.set_xticks(np.arange(0.9,1.2,0.1))
+ax2.set_yticks(np.arange(0,90,10))
 
-# plt.show()
+plt.show()
 
 #Passing
 
